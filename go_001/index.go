10b028c20940038fa3726e51ea7b548c3ae54d69
liqianchen <liqianchen@yoyohr.com>
2024-08-15 17:03:46 +0800

    fix: 完善两个数组的功能函数

--- go_001/index.go
+++ go_001/index.go
@@ -1,12 +1,26 @@
 package go_001
 
 // 查询数组中某个元素的下标
-func ListFindIndex(list []int, target int) int {
+type Operation func(item any) bool
+
+func FindIndex[T any](list []T, fu Operation) int {
 	index := -1
-	for i := 0; i < len(list); i++ {
-		if list[i] == target {
+	for i:= 0 ; i< len(list); i++{
+		if(fu(list[i])){
 			index = i
+			break
 		}
 	}
 	return index
+}
+
+// 删除数组最后一项并返回删除的元素
+func Pop[T any](list *[]T)(*T){
+	if(len(*list) == 0){
+		return nil
+	}else{
+		last := (*list)[len(*list) - 1]
+		*list = (*list)[:len(*list) - 1]
+		return &last
+	}
 }
--- index.go
+++ index.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"main/go_001"
+)
 func main() {
-
+	list := []int{1,2,3,4,5}
+	index := go_001.FindIndex(list,func (item any) bool {
+		return item == 1 
+	})
+	fmt.Println(index)
 }
